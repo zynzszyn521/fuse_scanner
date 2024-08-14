014fd41ce0690308dfe88aa8ca9b77ff1cbe0248
Allen <allen.yn.zhao@foxconn.com>
2024-08-14 08:29:05 +0800

    aa

--- android/src/main/kotlin/com/fuse/scanner/fuse_scanner/FuseScannerPlugin.kt
+++ android/src/main/kotlin/com/fuse/scanner/fuse_scanner/FuseScannerPlugin.kt
@@ -21,6 +21,7 @@ class FuseScannerPlugin: FlutterPlugin, MethodCallHandler {
   private val HONEYWELL_SCAN_ACTION = "com.honeywell.decode.intent.action.EDIT_DATA"
   private val BARCODE_DATA_ACTION   = "com.ehsy.warehouse.action.BARCODE_DATA"
   private val IDATA_SCAN_ACTION    = "android.intent.action.SCANRESULT"
+  private val ZEBRA_SCAN_ACTION    = "com.symbol.datawedge.data_string"
   private val CHANNEL_NAME = "com.fuse.scanner/methods"
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -57,6 +58,10 @@ class FuseScannerPlugin: FlutterPlugin, MethodCallHandler {
     val iDataIntentFilter   = IntentFilter()
     iDataIntentFilter.addAction(IDATA_SCAN_ACTION)
     context.registerReceiver(myBroadcastReceiver, iDataIntentFilter)
+
+    val zebraIntentFilter   = IntentFilter()
+    zebraIntentFilter.addAction(ZEBRA_SCAN_ACTION)
+    context.registerReceiver(myBroadcastReceiver, zebraIntentFilter)
   }
 
   private fun unregisterReceiver(context: Context) {
@@ -72,6 +77,14 @@ class FuseScannerPlugin: FlutterPlugin, MethodCallHandler {
           data = intent.getStringExtra("data").toString();
         }else if(action==IDATA_SCAN_ACTION){
           data = intent.getStringExtra("value").toString();
+        }else if(action==ZEBRA_SCAN_ACTION){
+          data = intent.getStringExtra("com.symbol.datawedge.data_string").toString();
+        }
+        val extras = intent.extras
+        if (extras != null) {
+            for (key in extras.keySet()) {
+                Log.i("Allen", "Extra key: $key, value: ${extras.get(key)}")
+            }
         }
         // 将扫描结果数据发送给 Flutter 侧进行处理
         Log.i("Allen","onScanResult: $data")
