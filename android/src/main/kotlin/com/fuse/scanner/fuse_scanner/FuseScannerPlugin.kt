0ede6fa680f270fa274329f8e10d227b5d3b9d9d
allen <liubaidukongjian@126.com>
2026-03-07 22:18:00 +0800

    a

--- android/src/main/kotlin/com/fuse/scanner/fuse_scanner/FuseScannerPlugin.kt
+++ android/src/main/kotlin/com/fuse/scanner/fuse_scanner/FuseScannerPlugin.kt
@@ -60,9 +60,9 @@ class FuseScannerPlugin: FlutterPlugin, MethodCallHandler {
     iDataIntentFilter.addAction(IDATA_SCAN_ACTION)
     context.registerReceiver(myBroadcastReceiver, iDataIntentFilter)
 
-     val inengoIntentFilter   = IntentFilter()
-    inengoIntentFilter.addAction(INVENGO_SCAN_ACTION)
-    context.registerReceiver(myBroadcastReceiver, inengoIntentFilter)
+    val invengoIntentFilter   = IntentFilter()
+    invengoIntentFilter.addAction(INVENGO_SCAN_ACTION)
+    context.registerReceiver(myBroadcastReceiver, invengoIntentFilter)
 
     val zebraIntentFilter   = IntentFilter()
     zebraIntentFilter.addAction(ZEBRA_SCAN_ACTION)
